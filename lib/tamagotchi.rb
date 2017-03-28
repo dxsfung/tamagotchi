f387585d08f120ff0929fe3fd58646e9c12f1ec3
dxsfung <davxsfung@gmail.com>
2017-03-29 06:07:43 +0800

    this commit will: add overfeed limit to 30

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -44,7 +44,7 @@ class Tamagotchi
     end
 
     define_method(:is_alive) do
-        if @food_level > 0
+        if @food_level > 0  && @food_level < 30
             return true
         else
             return false
